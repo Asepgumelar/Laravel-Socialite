37450fca9bfcf270c4f9e703becc2cd080cecbb6
Asep Gumelar <48475205+Asepgumelar@users.noreply.github.com>
2020-11-17 17:27:09 +0700

    base

---
+++ config/socialite.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = [
+
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_BASE_URL')
+    ],
+];
